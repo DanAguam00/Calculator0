4d4b865a802bb4ecd40cc891a503499ce16cb360
DanAguam00 <82425604+DanAguam00@users.noreply.github.com>
2021-06-27 17:32:14 +0800

    2.

---
+++ Calculator0/ReferenceClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator0
+{
+    class ReferenceClass
+    {
+        private String operation = "";
+        private Boolean operationPressed = false;
+        private String num0 = "";
+        private String num1 = "";
+
+        public string Operation { get => operation; set => operation = value; }
+        public bool OperationPressed { get => operationPressed; set => operationPressed = value; }
+        public string Num0 { get => num0; set => num0 = value; }
+        public string Num1 { get => num1; set => num1 = value; } 
+    }
+}
