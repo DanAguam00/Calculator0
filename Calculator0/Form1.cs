d838116ead3b47391f5a03ba45443dbc8a4c79db
DanAguam00 <82425604+DanAguam00@users.noreply.github.com>
2021-06-27 17:31:34 +0800

    1.

--- Calculator0/Form1.cs
+++ Calculator0/Form1.cs
@@ -12,10 +12,15 @@ namespace Calculator0
 {
     public partial class Calculator : Form
     {
+<<<<<<< Updated upstream
         String operation = "";
         Boolean operationPressed = false;
         String num0 = "";
         String num2 = "";
+=======
+        ReferenceClass refer = new ReferenceClass();
+        operationClass ops = new operationClass();
+>>>>>>> Stashed changes
 
         public Calculator()
         {
@@ -26,6 +31,7 @@ namespace Calculator0
         {
             TxtBx_1.Text = TxtBx_1.Text + " = ";
 
+<<<<<<< Updated upstream
             if (operation.Equals("+"))
             {
                 TxtBx_2.Text = (float.Parse(num0) + float.Parse(TxtBx_2.Text)).ToString();
@@ -41,6 +47,31 @@ namespace Calculator0
             else if (operation.Equals("/"))
             {
                 TxtBx_2.Text = (float.Parse(num0)  / float.Parse(TxtBx_2.Text)).ToString();
+=======
+            if (refer.Operation.Equals("+"))
+            {
+                refer.Num1 = TxtBx_2.Text;
+                ops.Add();
+                TxtBx_2.Text = refer.Num1;
+            }
+            else if (refer.Operation.Equals("-"))
+            {
+                refer.Num1 = TxtBx_2.Text;
+                ops.Subtract();
+                TxtBx_2.Text = refer.Num1;
+            }
+            else if (refer.Operation.Equals("*"))
+            {
+                refer.Num1 = TxtBx_2.Text;
+                ops.Multiply();
+                TxtBx_2.Text = refer.Num1;
+            }
+            else if (refer.Operation.Equals("/"))
+            {
+                refer.Num1 = TxtBx_2.Text;
+                ops.Divide();
+                TxtBx_2.Text = refer.Num1;
+>>>>>>> Stashed changes
             }
 
             TxtBx_1.Text = TxtBx_1.Text + TxtBx_2.Text;
@@ -48,6 +79,7 @@ namespace Calculator0
 
         private void Btn_0_Click(object sender, EventArgs e)
         {
+<<<<<<< Updated upstream
             if (operationPressed)
             {
                 num0 = TxtBx_2.Text;
@@ -57,10 +89,14 @@ namespace Calculator0
             TxtBx_1.Text = TxtBx_1.Text + "0";
             TxtBx_2.Text = TxtBx_2.Text + "0";
             operationPressed = false;
+=======
+            displayCharacter("0");
+>>>>>>> Stashed changes
         }
 
         private void Btn_1_Click(object sender, EventArgs e)
         {
+<<<<<<< Updated upstream
             if (operationPressed)
             {
                 num0 = TxtBx_2.Text;
@@ -70,10 +106,14 @@ namespace Calculator0
             TxtBx_1.Text = TxtBx_1.Text + "1";
             TxtBx_2.Text = TxtBx_2.Text + "1";
             operationPressed = false;
+=======
+            displayCharacter("1");
+>>>>>>> Stashed changes
         }
 
         private void Btn_2_Click(object sender, EventArgs e)
         {
+<<<<<<< Updated upstream
             if (operationPressed)
             {
                 num0 = TxtBx_2.Text;
@@ -83,10 +123,14 @@ namespace Calculator0
             TxtBx_1.Text = TxtBx_1.Text + "2";
             TxtBx_2.Text = TxtBx_2.Text + "2";
             operationPressed = false;
+=======
+            displayCharacter("2");
+>>>>>>> Stashed changes
         }
 
         private void Btn_3_Click(object sender, EventArgs e)
         {
+<<<<<<< Updated upstream
             if (operationPressed)
             {
                 num0 = TxtBx_2.Text;
@@ -96,10 +140,14 @@ namespace Calculator0
             TxtBx_1.Text = TxtBx_1.Text + "3";
             TxtBx_2.Text = TxtBx_2.Text + "3";
             operationPressed = false;
+=======
+            displayCharacter("3");
+>>>>>>> Stashed changes
         }
 
         private void Btn_4_Click(object sender, EventArgs e)
         {
+<<<<<<< Updated upstream
             if (operationPressed)
             {
                 num0 = TxtBx_2.Text;
@@ -109,10 +157,14 @@ namespace Calculator0
             TxtBx_1.Text = TxtBx_1.Text + "4";
             TxtBx_2.Text = TxtBx_2.Text + "4";
             operationPressed = false;
+=======
+            displayCharacter("5");
+>>>>>>> Stashed changes
         }
 
         private void Btn_5_Click(object sender, EventArgs e)
         {
+<<<<<<< Updated upstream
             if (operationPressed)
             {
                 num0 = TxtBx_2.Text;
@@ -122,10 +174,14 @@ namespace Calculator0
             TxtBx_1.Text = TxtBx_1.Text + "5";
             TxtBx_2.Text = TxtBx_2.Text + "5";
             operationPressed = false;
+=======
+            displayCharacter("5");
+>>>>>>> Stashed changes
         }
 
         private void Btn_6_Click(object sender, EventArgs e)
         {
+<<<<<<< Updated upstream
             if (operationPressed)
             {
                 num0 = TxtBx_2.Text;
@@ -134,10 +190,14 @@ namespace Calculator0
 
             TxtBx_1.Text = TxtBx_1.Text + "6";
             TxtBx_2.Text = TxtBx_2.Text + "6";
+=======
+            displayCharacter("6");
+>>>>>>> Stashed changes
         }
 
         private void Btn_7_Click(object sender, EventArgs e)
         {
+<<<<<<< Updated upstream
             if (operationPressed)
             {
                 num0 = TxtBx_2.Text;
@@ -147,10 +207,14 @@ namespace Calculator0
             TxtBx_1.Text = TxtBx_1.Text + "7";
             TxtBx_2.Text = TxtBx_2.Text + "7";
             operationPressed = false;
+=======
+            displayCharacter("7");
+>>>>>>> Stashed changes
         }
 
         private void Btn_8_Click(object sender, EventArgs e)
         {
+<<<<<<< Updated upstream
             if (operationPressed)
             {
                 num0 = TxtBx_2.Text;
@@ -160,10 +224,14 @@ namespace Calculator0
             TxtBx_1.Text = TxtBx_1.Text + "8";
             TxtBx_2.Text = TxtBx_2.Text + "8";
             operationPressed = false;
+=======
+            displayCharacter("8");
+>>>>>>> Stashed changes
         }
 
         private void Btn_9_Click(object sender, EventArgs e)
         {
+<<<<<<< Updated upstream
             if(operationPressed)
             {
                 num0 = TxtBx_2.Text;
@@ -173,41 +241,81 @@ namespace Calculator0
             TxtBx_1.Text = TxtBx_1.Text + "9";
             TxtBx_2.Text = TxtBx_2.Text + "9";
             operationPressed = false;
+=======
+            displayCharacter("9");
+>>>>>>> Stashed changes
         }
 
         private void Btn_Plus_Click(object sender, EventArgs e)
         {
             TxtBx_1.Text = TxtBx_1.Text + " + ";
+<<<<<<< Updated upstream
             operation = "+";
             operationPressed = true;
+=======
+            refer.Operation = "+";
+            refer.OperationPressed = true;
+>>>>>>> Stashed changes
         }
 
         private void Btn_Minus_Click(object sender, EventArgs e)
         {
             TxtBx_1.Text = TxtBx_1.Text + " - ";
+<<<<<<< Updated upstream
             operation = "-";
             operationPressed = true;
+=======
+            refer.Operation = "-";
+            refer.OperationPressed = true;
+>>>>>>> Stashed changes
         }
 
         private void Btn_Multiply_Click(object sender, EventArgs e)
         {
             TxtBx_1.Text = TxtBx_1.Text + " * ";
+<<<<<<< Updated upstream
             operation = "*";
             operationPressed = true;
+=======
+            refer.Operation = "*";
+            refer.OperationPressed = true;
+>>>>>>> Stashed changes
         }
 
         private void Btn_Dvd_Click(object sender, EventArgs e)
         {
             TxtBx_1.Text = TxtBx_1.Text + " / ";
+<<<<<<< Updated upstream
             operation = "/";
             operationPressed = true;
+=======
+            refer.Operation = "/";
+            refer.OperationPressed = true;
+>>>>>>> Stashed changes
         }
 
         private void Btn_Dot_Click(object sender, EventArgs e)
         {
+<<<<<<< Updated upstream
             TxtBx_1.Text = TxtBx_1.Text + ".";
             TxtBx_2.Text = TxtBx_2.Text + ".";
             operationPressed = false;
+=======
+            displayCharacter(".");
+        }
+
+        private void displayCharacter(String character)
+        {
+            if (refer.OperationPressed)
+            {
+                refer.Num0 = TxtBx_2.Text;
+                TxtBx_2.Text = "";
+            }
+
+            TxtBx_1.Text = TxtBx_1.Text + character;
+            TxtBx_2.Text = TxtBx_2.Text + character;
+            refer.OperationPressed = false;
+>>>>>>> Stashed changes
         }
     }
 }
